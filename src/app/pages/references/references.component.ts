799f2778bcc1b2d9a8e2c33dcf4b06208a91b7c5
tahaerden <tahaerden@windowslive.com>
2023-08-20 18:52:10 +0300

    Added content to References.

--- src/app/pages/references/references.component.ts
+++ src/app/pages/references/references.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./references.component.scss']
 })
 export class ReferencesComponent {
-
+  references = [
+    'ankara-buyuksehir-belediyesi.jpeg',
+    'asfat.jpeg',
+    'aydin-buyuksehir-belediyesi.jpeg',
+    'cankaya-belediyesi.jpeg',
+    'gebze-belediyesi.jpeg',
+    'icisleri-bakanligi.jpeg',
+    'kastamonu-universitesi.jpeg',
+    'malatya-valiligi.jpeg',
+    'msb.jpeg',
+    'saglik-bakanligi.jpeg',
+    'tobb.jpeg',
+    'toki.jpeg'
+  ];
 }
