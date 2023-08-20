69ad76e03aef9397aee29c3227a56a8e17a92a7a
tahaerden <tahaerden@windowslive.com>
2023-08-20 10:06:53 +0300

    Added translations for 404 page.

--- src/app/pages/page-not-found/page-not-found.module.ts
+++ src/app/pages/page-not-found/page-not-found.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { PageNotFoundRoutingModule } from './page-not-found-routing.module';
 import { PageNotFoundComponent } from './page-not-found.component';
-
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
-  declarations: [
-    PageNotFoundComponent
-  ],
-  imports: [
-    CommonModule,
-    PageNotFoundRoutingModule
-  ]
+  declarations: [PageNotFoundComponent],
+  imports: [CommonModule, PageNotFoundRoutingModule, SharedModule]
 })
-export class PageNotFoundModule { }
+export class PageNotFoundModule {}
