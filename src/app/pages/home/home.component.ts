2b8c62a40f0e2ae5d7ba742a9d0d7a3429b689c3
tahaerden <tahaerden@windowslive.com>
2023-08-23 12:07:15 +0300

    Re-ordered nav items. Set accent color.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -17,15 +17,15 @@ export class HomeComponent {
       src: 'https://images.unsplash.com/photo-1567653418876-5bb0e566e1c2?ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'
     },
     {
-      headline: 'Miouw',
+      headline: 'Güven, Yenilik, Profesyonellik',
       src: 'https://images.unsplash.com/photo-1559181567-c3190ca9959b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80'
     },
     {
-      headline: 'In The Wilderness',
+      headline: 'Güven, Yenilik, Profesyonellik',
       src: 'https://images.unsplash.com/photo-1557800634-7bf3c7305596?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2001&q=80'
     },
     {
-      headline: 'Focus On The Writing',
+      headline: 'Güven, Yenilik, Profesyonellik',
       src: 'https://images.unsplash.com/photo-1551410224-699683e15636?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80'
     }
   ];
