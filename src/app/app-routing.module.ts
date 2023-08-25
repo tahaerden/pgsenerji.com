e18c836679bd7f13fe21632035c0709d71a0d8e9
tahaerden <tahaerden@windowslive.com>
2023-08-25 20:28:31 +0300

    Implemented route animations.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,41 +5,42 @@ import { LayoutComponent } from './core/components/layout/layout.component';
 const routes: Routes = [
   {
     path: '',
-    component: LayoutComponent,
-    children: [
-      {
-        path: '',
-        loadChildren: () => import('./pages/home/home.module').then((m) => m.HomeModule)
-      },
-      {
-        path: 'solution-partners',
-        loadChildren: () =>
-          import('./pages/solution-partners/solution-partners.module').then(
-            (m) => m.SolutionPartnersModule
-          )
-      },
-      {
-        path: 'about-us',
-        loadChildren: () => import('./pages/about-us/about-us.module').then((m) => m.AboutUsModule)
-      },
-      {
-        path: 'references',
-        loadChildren: () =>
-          import('./pages/references/references.module').then((m) => m.ReferencesModule)
-      },
-      {
-        path: 'services',
-        loadChildren: () => import('./pages/services/services.module').then((m) => m.ServicesModule)
-      },
-      {
-        path: 'projects',
-        loadChildren: () => import('./pages/projects/projects.module').then((m) => m.ProjectsModule)
-      },
-      {
-        path: 'contact',
-        loadChildren: () => import('./pages/contact/contact.module').then((m) => m.ContactModule)
-      }
-    ]
+    data: { animation: 'home' },
+    loadChildren: () => import('./pages/home/home.module').then((m) => m.HomeModule)
+  },
+  {
+    path: 'solution-partners',
+    data: { animation: 'solution-partners' },
+    loadChildren: () =>
+      import('./pages/solution-partners/solution-partners.module').then(
+        (m) => m.SolutionPartnersModule
+      )
+  },
+  {
+    path: 'about-us',
+    data: { animation: 'about-us' },
+    loadChildren: () => import('./pages/about-us/about-us.module').then((m) => m.AboutUsModule)
+  },
+  {
+    path: 'references',
+    data: { animation: 'references' },
+    loadChildren: () =>
+      import('./pages/references/references.module').then((m) => m.ReferencesModule)
+  },
+  {
+    path: 'services',
+    data: { animation: 'services' },
+    loadChildren: () => import('./pages/services/services.module').then((m) => m.ServicesModule)
+  },
+  {
+    path: 'projects',
+    data: { animation: 'projects' },
+    loadChildren: () => import('./pages/projects/projects.module').then((m) => m.ProjectsModule)
+  },
+  {
+    path: 'contact',
+    data: { animation: 'contact' },
+    loadChildren: () => import('./pages/contact/contact.module').then((m) => m.ContactModule)
   },
   {
     path: '404',
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,52 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { animate, group, query, style, transition, trigger } from '@angular/animations';
+import { ChildrenOutletContexts } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('routeAnimations', [
+      transition('* <=> *', [
+        style({ position: 'relative' }),
+        query(
+          ':enter, :leave',
+          [
+            style({
+              position: 'absolute',
+              top: '100px',
+              left: 0,
+              width: '100%'
+            })
+          ],
+          { optional: true }
+        ),
+        query(':enter', [style({ opacity: 0, transform: 'scale(0.8)' })], { optional: true }),
+        group([
+          query(
+            ':leave',
+            [animate('300ms ease-out', style({ opacity: 0, transform: 'scale(0.8)' }))],
+            { optional: true }
+          ),
+          query(
+            ':enter',
+            [animate('600ms ease-out', style({ opacity: 1, transform: 'scale(1)' }))],
+            { optional: true }
+          )
+        ])
+      ])
+    ])
+  ]
 })
 export class AppComponent {
   title = 'PGS Enerji';
-  constructor(translate: TranslateService) {
+  constructor(translate: TranslateService, private contexts: ChildrenOutletContexts) {
     translate.setDefaultLang('tr');
     translate.use('tr');
+  }
+  getRouteAnimationData() {
+    return this.contexts.getContext('primary')?.route?.snapshot?.data?.['animation'];
   }
 }
