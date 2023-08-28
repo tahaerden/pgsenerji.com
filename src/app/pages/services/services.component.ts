9f00e5f16ec38a7c4881e81d9a5fbd0c6f1980d9
Taha Erden <tahaerden@windowslive.com>
2023-08-28 16:42:11 +0300

    Added content to services page.

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./services.component.scss']
 })
 export class ServicesComponent {
-
+  services = [
+    { route: 'lighting-services', translationKey: 'LIGHTING_SERVICES' },
+    { route: 'cctv', translationKey: 'CCTV' },
+    { route: 'compensation-systems', translationKey: 'COMPENSATION_SYSTEMS' },
+    {
+      route: 'engineering-services-and-projects',
+      translationKey: 'ENGINEERING_SERVICES_AND_PROJECTS'
+    },
+    { route: 'og-facilities', translationKey: 'OG_FACILITIES' },
+    { route: 'automation', translationKey: 'AUTOMATION' },
+    { route: 'lightning-rod-and-grounding', translationKey: 'LIGHTNING_ROD_AND_GROUNDING' },
+    { route: 'landscape-lighting', translationKey: 'LANDSCAPE_LIGHTING' },
+    { route: 'plc-systems', translationKey: 'PLC_SYSTEMS' },
+    { route: 'transformers', translationKey: 'TRANSFORMERS' }
+  ];
 }
