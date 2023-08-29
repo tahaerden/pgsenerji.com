32f7e8baa00f9415ad249b1089101deee6b1a51d
Taha Erden <tahaerden@windowslive.com>
2023-08-29 14:41:10 +0300

    Added safePipe. Added iframe video to carousel.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -13,21 +13,21 @@ export class HomeComponent {
   animationType = AnimationType.Fade;
   slides: Slide[] = [
     {
-      headline: 'Güven, Yenilik, Profesyonellik',
-      src: 'https://images.unsplash.com/photo-1567653418876-5bb0e566e1c2?ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'
-    },
-    {
-      headline: 'Güven, Yenilik, Profesyonellik',
-      src: 'https://images.unsplash.com/photo-1559181567-c3190ca9959b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80'
-    },
-    {
-      headline: 'Güven, Yenilik, Profesyonellik',
-      src: 'https://images.unsplash.com/photo-1557800634-7bf3c7305596?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2001&q=80'
-    },
-    {
-      headline: 'Güven, Yenilik, Profesyonellik',
-      src: 'https://images.unsplash.com/photo-1551410224-699683e15636?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80'
+      headline: 'test',
+      src: 'https://www.youtube.com/embed/bFDNrQp7vi4'
     }
+    // {
+    //   headline: 'Güven, Yenilik, Profesyonellik',
+    //   src: 'https://www.youtube.com/embed/bFDNrQp7vi4?autoplay=1&mute=1&loop=1&playlist=bFDNrQp7vi4&controls=0&rel=0&modestbranding=0'
+    // },
+    // {
+    //   headline: 'TODO: Projeler slogan',
+    //   src: 'https://www.youtube.com/clip/UgkxVvV2RMxVcvwYgNql10T7osDr1Drakj45?si=Eq5PFtcDGSETbDYg'
+    // },
+    // {
+    //   headline: 'TODO: Çalışanlar slogan',
+    //   src: 'https://www.youtube.com/clip/UgkxaaXVwmRcTxtvuVBWq_gtH3QKX4Ki_-5y?si=upWGY6OER5NcrPcq'
+    // }
   ];
   projects: Slide[] = [
     {
