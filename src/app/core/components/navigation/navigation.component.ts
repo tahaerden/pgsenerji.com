0d5a18aa6b3f4bb33a597e0be33069c10853dd19
tahaerden <tahaerden@windowslive.com>
2023-09-11 13:38:22 +0300

    Changed nav style for Home page. Used transparent logo in nav.

--- src/app/core/components/navigation/navigation.component.ts
+++ src/app/core/components/navigation/navigation.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-navigation',
@@ -9,8 +11,15 @@ import { TranslateService } from '@ngx-translate/core';
 export class NavigationComponent {
   showProfileMenu = false;
   selectedLang = this.translate.getDefaultLang();
+  isHomePage = false;
 
-  constructor(private translate: TranslateService) {}
+  constructor(private translate: TranslateService, private router: Router) {
+    router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        this.isHomePage = router.url === '/' ? true : false;
+      });
+  }
 
   switchLanguage(selectedLang: string) {
     this.translate.use(selectedLang);
