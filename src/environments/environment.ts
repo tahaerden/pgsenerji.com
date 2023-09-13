0c48391e8471d05d30b375bf3a36540981c189ae
tahaerden <tahaerden@windowslive.com>
2023-09-13 16:48:15 +0300

    Implemented OSM map in Projects page.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,8 @@
 export const environment = {
   // AWS COGNITO
   cognitoUserPoolId: '',
-  cognitoAppClientId: ''
+  cognitoAppClientId: '',
+  // UMAP OSM
+  editLink:
+    'https://umap.openstreetmap.fr/en/map/anonymous-edit/959542:iIBA0tjwvT2B89sz2BOIDa3Ifqe_S7Ia0lh5iVi-jmQ'
 };
